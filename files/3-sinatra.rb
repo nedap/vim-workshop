fbe5155756efdd356c79e4432073d575e2dec26d
Joel Stemmer <stemmertech@gmail.com>
2012-09-07 07:46:33 +0200

    Add test files to play with

---
+++ files/3-sinatra.rb
@@ -0,0 +1,22 @@
+# myapp.rb
+require 'sinatra'
+
+get '/' do
+  'Hello world!'
+end
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params[:name] is 'foo' or 'bar'
+  "Hello #{params[:name]}!"
+end
+
+set(:probability) { |value| condition { rand <= value } }
+
+get '/win_a_car', :probability => 0.1 do
+  "You won!"
+end
+
+get '/win_a_car' do
+  "Sorry, you lost."
+end
